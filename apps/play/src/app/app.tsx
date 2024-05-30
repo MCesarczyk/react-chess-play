afd4bb0062888dbc5fd70b75991c0be1cbec07d5
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2024-05-31 01:06:48 +0200

    feat: create frontend (#1)
    * feat(play): scaffold react app
    
    * feat(app): create chessboard with sample figures
    
    * feat(play): install react-dnd, add knight images, leave only black knight on board
    
    * feat(play): implement knights move
    
    * feat(knight): add dragging effect
    
    * feat(knight): implement drop action

---
+++ apps/play/src/app/app.tsx
@@ -0,0 +1,185 @@
+import { ReactElement, useState } from 'react';
+import clsx from 'clsx';
+import { useDrag, useDrop } from 'react-dnd';
+
+import king from '../assets/king.png';
+import pawn from '../assets/pawn.png';
+import knight from '../assets/knight_b.png';
+
+export type Coord = [number, number];
+
+export type PieceRecord = {
+  type: PieceType;
+  location: Coord;
+};
+
+export type PieceType = 'king' | 'pawn' | 'knight';
+
+type PieceProps = {
+  type: PieceType;
+  image: string;
+  alt: string;
+};
+
+export function isEqualCoord(c1: Coord, c2: Coord): boolean {
+  return c1[0] === c2[0] && c1[1] === c2[1];
+}
+
+function Square({
+  row,
+  col,
+  piece,
+  onSquareClick,
+}: {
+  row: number;
+  col: number;
+  piece: PieceRecord | undefined;
+  onSquareClick: (coord: Coord) => void;
+}) {
+  const isDark = (row + col) % 2 === 1;
+  const squareCoord: Coord = [row, col];
+
+  const [{ isOver }, drop] = useDrop(
+    () => ({
+      accept: 'knight',
+      drop: () => onSquareClick(squareCoord),
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      }),
+    }),
+    [row, col]
+  );
+
+  const pieceLookup: {
+    [Key in PieceType]: () => ReactElement;
+  } = {
+    king: () => <King />,
+    pawn: () => <Pawn />,
+    knight: () => <Knight />,
+  };
+
+  return (
+    <div
+      key={`${row}-${col}`}
+      ref={drop}
+      className={clsx(
+        'w-full h-full grid place-items-center',
+        isDark ? 'bg-gray-700' : 'bg-white',
+        isOver && 'bg-yellow-300'
+      )}
+      onClick={() => onSquareClick(squareCoord)}
+    >
+      {piece && pieceLookup[piece.type]()}
+    </div>
+  );
+}
+
+function renderSquares(
+  pieces: PieceRecord[],
+  onSquareClick: (coord: Coord) => void
+) {
+  const squares = [];
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      const squareCoord: Coord = [row, col];
+
+      const piece = pieces.find((piece) =>
+        isEqualCoord(piece.location, squareCoord)
+      );
+
+      squares.push(
+        <Square
+          key={`${row}-${col}`}
+          {...{
+            row,
+            col,
+            piece,
+            onSquareClick,
+          }}
+        />
+      );
+    }
+  }
+  return squares;
+}
+
+export function canMoveKnight(from: Coord, to: Coord) {
+  const dx = to[0] - from[0];
+  const dy = to[1] - from[1];
+
+  return (
+    (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+    (Math.abs(dx) === 1 && Math.abs(dy) === 2)
+  );
+}
+
+export function App() {
+  const [pieces, setPieces] = useState<PieceRecord[]>([
+    // { type: 'king', location: [3, 2] },
+    // { type: 'pawn', location: [1, 6] },
+    { type: 'knight', location: [4, 4] },
+  ]);
+
+  function movePiece(pieces: PieceRecord[], piece: PieceType, to: Coord): void {
+    const matchingPiece = pieces.find((p) => p.type === piece);
+    const restPieces = pieces.filter((p) => p.type !== piece);
+
+    setPieces(
+      matchingPiece
+        ? [...restPieces, { ...matchingPiece, location: to }]
+        : pieces
+    );
+  }
+
+  const knight = pieces.find((p) => p.type === 'knight');
+
+  const handleSquareClick = (coord: Coord) => {
+    if (knight && canMoveKnight(knight.location, coord)) {
+      console.log('Moving knight to:', coord);
+
+      movePiece(pieces, 'knight', coord);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-8 grid-rows-8 w-max h-max max-w-xl aspect-square border-4 border-gray-500">
+      {renderSquares(pieces, handleSquareClick)}
+    </div>
+  );
+}
+
+function Piece({ type, image, alt }: PieceProps) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <img
+      ref={drag}
+      className={clsx(
+        'p-1 m-2 rounded-lg shadow-xl bg-gray-400 hover:bg-gray-600',
+        isDragging && 'opacity-50 bg-opacity-50 border-2'
+      )}
+      height="80%"
+      width="80%"
+      src={image}
+      alt={alt}
+      draggable="false"
+    />
+  ); // draggable set to false to prevent dragging of the images
+}
+
+export function King() {
+  return <Piece type="king" image={king} alt="King" />;
+}
+
+export function Pawn() {
+  return <Piece type="pawn" image={pawn} alt="Pawn" />;
+}
+
+export function Knight() {
+  return <Piece type="knight" image={knight} alt="Knight" />;
+}
---
+++ apps/play/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+import { App } from './app/app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <StrictMode>
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
+  </StrictMode>
+);
