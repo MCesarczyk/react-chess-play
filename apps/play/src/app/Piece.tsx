30a57c5f65e88f7471ddf14323d2da67d3c5cabf
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-06-02 23:43:08 +0200

    feat(piece): update style to avoid board squares deformation

--- apps/play/src/app/Piece.tsx
+++ apps/play/src/app/Piece.tsx
@@ -12,8 +12,8 @@ export function Piece({ type, image, alt, canMovePiece }: PieceData) {
     <img
       ref={setNodeRef}
       className={clsx(
-        'p-1 m-2 rounded-lg shadow-xl bg-gray-400 hover:bg-gray-600',
-        isDragging && 'opacity-50 bg-opacity-50 border-2'
+        'w-3/4 aspect-square p-1 rounded-lg shadow-xl bg-gray-400 hover:bg-gray-600',
+        isDragging && 'opacity-50 bg-opacity-50 border-1'
       )}
       height="80%"
       width="80%"
--- apps/play/src/app/Square.tsx
+++ apps/play/src/app/Square.tsx
@@ -27,9 +27,11 @@ export const Square = forwardRef(
         style={{
           width: '100%',
           height: '100%',
+          aspectRatio: '1 / 1',
           display: 'grid',
           placeItems: 'center',
           backgroundColor: bgColor,
+          border: '1px solid #000',
         }}
       >
         {children}
