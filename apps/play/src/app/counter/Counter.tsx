de377f94936c799b7b1e24d6da5b207e1aa23d46
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-06-10 03:26:46 +0200

    feat(counter): add icons to pieces list

--- apps/play/src/app/counter/Counter.tsx
+++ apps/play/src/app/counter/Counter.tsx
@@ -15,25 +15,49 @@ export const Counter = ({ capturedPieces, colour }: CounterProps) => {
     return currentTypes.filter((t) => t === type).length;
   };
 
+  const getIconByType = (type: string) => {
+    return currentPieces.find((p) => p.type === type)?.icon;
+  };
+
   return (
     <CounterWrapper>
-      <h2>Captured pieces:</h2>
-      <h3>{colour}</h3>
-      <ul>
+      <Header>{colour.toUpperCase()}</Header>
+      <PiecesList>
         {distinctTypes.map((type) => (
           <li key={type}>
-            {type} - {calculateTypeOccurencies(type)}
+            {getIconByType(type)} - {calculateTypeOccurencies(type)}
           </li>
         ))}
-      </ul>
+      </PiecesList>
     </CounterWrapper>
   );
 };
 
 const CounterWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 1rem;
   align-items: center;
   flex-grow: 1;
   padding: 1rem;
+`;
+
+const Header = styled.h2`
+  flex-grow: 1;
+  text-align: center;
+`;
+
+const PiecesList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  font-size: 1.5rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 1rem;
+
+  @media screen and (min-width: 1024px) {
+    flex-direction: column;
+  }
 `;
